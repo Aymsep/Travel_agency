d8fb3e90bb961342512ef566f13ef1501232c8dc
Aymsep <71574687+Aymsep@users.noreply.github.com>
2023-03-17 19:01:46 +0100

    added the booking section above header

--- src/Container/Header/Header.jsx
+++ src/Container/Header/Header.jsx
@@ -3,6 +3,7 @@ import './Header.css'
 import image from '../../assets/header_img.png'
 import Button from '../../components/Button/Button'
 import {HiArrowNarrowRight} from 'react-icons/hi'
+import Book from '../../components/Book_Input/Book'
 
 const Header = () => {
   return (
@@ -11,13 +12,14 @@ const Header = () => {
         <div className="app__header-left">
             <h1>Personalized trip <br /> in minutes. <span>Only $4.99</span> </h1>
             <p>Based on your occasion, style and budget. It includes recommendations for travel, stay, and activities.</p>
-            <Button text="plan a trip"  icon={<HiArrowNarrowRight/>}/>
-            
+            {/* <Button text="plan a trip"  icon={<HiArrowNarrowRight/>}/>     */}
+            <Book/>
         </div>
 
         <div className="app__header-right">
             <img src={image} alt="" />
         </div>
+
 
     </div>
   )
