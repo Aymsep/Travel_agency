d4a11cd7667028a39318b7a1203ea38cf175495c
Aymsep <71574687+Aymsep@users.noreply.github.com>
2023-03-17 21:42:16 +0100

    finish header section , start with discover section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Container/Header/Header'
 import Navbar from './components/Navbar/Navbar'
+import Discover from './Container/Discover/Discover'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Header/>
+      <Discover/>
     </div>
   )
 }
---
+++ src/Container/Discover/Discover.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Discover.css'
+
+const Discover = () => {
+  return (
+    <div className="app__discover">
+        <h1>Discover Weekly</h1>
+        <p className="p-text">Explore new travel inspiration updated every Monday</p>
+    </div>
+  )
+}
+
+export default Discover
